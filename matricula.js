7861d7f62d22af886932aeda393b37b456c47a0b
Willian Telpes <150863141+Telpes@users.noreply.github.com>
2023-11-22 17:10:19 -0300

    Add files via upload

--- matricula.js
+++ matricula.js
@@ -5,24 +5,24 @@ var totalHoras = 0;
 var totalCursos = 0;
 
 function adicionaCurso(checkbox) {
-
+    
     if (checkbox.checked) { //marquei
         totalCursos++;
         totalHoras += parseInt(checkbox.value);
-    } else {  //dermarquei
+    } else { //desmarquei
         totalCursos--;
         totalHoras -= parseInt(checkbox.value);
     }
 
-    tdTotalHoras.textContent = totalHoras + 'h(s)';
-    tdTotalCursos.textContent = totalCursos + 'curso(s)';
-    
+    tdTotalHoras.textContent = totalHoras + ' h(s)';
+    tdTotalCursos.textContent = totalCursos + ' curso(s)';
+
 }
 
 function confirmaMatricula() {
     if (totalCursos > 0) {
-        alert('Matricula confirmada');
-        window.location.href ="index.html";
+        alert('Matrícula confirmada');
+        window.location.href = "index.html";
     } else {
         alert('Nenhum curso selecionado!');
     }
